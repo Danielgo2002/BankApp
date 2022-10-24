ce29d2d1e39245c9513a99dd4f30fb5dc3925951
Danielgo2002 <91335477+Danielgo2002@users.noreply.github.com>
2022-10-24 16:44:51 +0300

    gg

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AccountsController } from './accounts/accounts.controller';
 import { AccountModule } from './accounts/accounts.module';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [AuthModule, AccountModule, MongooseModule.forRoot('mongodb://localhost/nest',{
     autoIndex :true,
-  })],
+  }), AdminModule],
 
 })
 export class AppModule {}
